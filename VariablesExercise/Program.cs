43a0981586a04c96dcb230f8a1990447b0019b33
joedupor <joedupor@gmail.com>
2023-08-24 18:50:25 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string programmerNickname = "Ace";
+            int programmerAge = 30;
+            char programmerID = 'Z';
+            bool isNew = false;
+            double hoursCoded = 2000.5;
+            decimal hoursGoal = 10000.0m;
+
+            Console.WriteLine($"There is a programming student nicknamed {programmerNickname} and their age is \n"  +
+                $"  {programmerAge}and they are also known here as  {programmerID}  and T/F that they are a new programmer:  {isNew} \n " +
+                $"and number of hours they have coded last semester is  {hoursCoded} " +
+                $"and finally their number of hours goal for \n" +
+                $" this semester is  {hoursGoal}!!!");
+
+            
+
         }
     }
 }
